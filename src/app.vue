aa41c76907ea074b21e541c1255dabaad082599a
EliasSchaut <eschaut@outlook.de>
2025-01-18 16:49:38 +0100

    add metadata

--- src/app.vue
+++ src/app.vue
@@ -1,27 +1,33 @@
 <template>
-  <Title>Elias Schaut</Title>
+  <Title>Kids Bday 2025</Title>
   <Meta
-      name="description"
-      content="Elias Schaut"
+    name="description"
+    content="Save the Date - Kids Bday is on June 7, 2025"
   />
-  <Meta name="keywords" content="elias" />
-  <Meta name="theme-color" content="#111827" />
+  <Meta
+    name="keywords"
+    content="bday, meta, kid, elias, event, karlsruhe, hadiko"
+  />
+  <Meta name="author" content="Elias Lorenz Schaut" />
+  <meta name="copyright" content="Elias Lorenz Schaut" />
+  <Meta name="theme-color" content="#4f46e5" />
+  <Meta name="image" content="/favicon/thumbnail.png" />
   <Link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/favicon/apple-touch-icon.png"
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="/favicon/apple-touch-icon.png"
   />
   <Link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/favicon/favicon-32x32.png"
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="/favicon/favicon-32x32.png"
   />
   <Link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="/favicon/favicon-16x16.png"
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="/favicon/favicon-16x16.png"
   />
   <Link rel="icon" href="/favicon/favicon.ico" />
   <Link rel="manifest" href="/site.webmanifest" />
